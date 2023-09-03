712cc2fd701b05d6ff43b63859232ffec4923b03
OH SUN CHUL <ispo3695@naver.com>
2023-09-04 00:24:09 +0900

    normal

--- client.cpp
+++ client.cpp
@@ -10,268 +10,232 @@
 #include <wchar.h>
 #include <locale.h>
 
-
-
-
 using namespace std;
-
-
-
 
 class ETH
 {
- 
 
-  private :
+private:
+    int sockfd;
+    struct sockaddr_in Addr_Server; // IP(L3 Header) , Port(L4 Header)
 
-   int sockfd; 
-   struct sockaddr_in Addr_Server; // IP(L3 Header) , Port(L4 Header)
-      
-    
- public :
+public:
+    // Constructor
+    ETH(char *, int); // Require Both IP Address & Port.
+    ~ETH();
+    void Transmit(char *);
+    char Receive();
+    void Receive(char *, int);
 
- // Constructor
- ETH(char* ,int);  // Require Both IP Address & Port. 
- ~ETH(); 
- void Transmit(char* );
- char Receive();
- void Receive(char* ,int);  
+    int GetSockFD();
 
- int GetSockFD();
-
-
-  ETH* Get_This(); 
-
-
+    ETH *Get_This();
 };
 
-ETH :: ETH(char* Address ,int Port)
+ETH ::ETH(char *Address, int Port)
 {
-    
-    char c; 
-    cout << "--- Construcing Ethernet Instance ---" << endl; 
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    
-    bzero(&Addr_Server,sizeof(Addr_Server));   
-    
-    Addr_Server.sin_family = AF_INET;  
-    Addr_Server.sin_port = htons(Port); 
-    Addr_Server.sin_addr.s_addr = inet_addr(Address); 
-  
-    if ( connect(sockfd, (struct sockaddr *)&Addr_Server, sizeof(Addr_Server)) == 0 )
+    char c;
+    cout << "--- Construcing Ethernet Instance ---" << endl;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&Addr_Server, sizeof(Addr_Server));
+
+    Addr_Server.sin_family = AF_INET;
+    Addr_Server.sin_port = htons(Port);
+    Addr_Server.sin_addr.s_addr = inet_addr(Address);
+
+    if (connect(sockfd, (struct sockaddr *)&Addr_Server, sizeof(Addr_Server)) == 0)
     {
 
-        cout << "--- Get SYN(1) + ACK(1) Flag From Server Sucessfully [ " << Address << " ] ---" << endl; 
-        cout << "--- Client Enters Establishment To Server [ " << Address << " ] ---" << endl; 
-
-
+        cout << "--- Get SYN(1) + ACK(1) Flag From Server Sucessfully [ " << Address << " ] ---" << endl;
+        cout << "--- Client Enters Establishment To Server [ " << Address << " ] ---" << endl;
     }
-     
-        
-
 }
 
-int ETH :: GetSockFD()
+int ETH ::GetSockFD()
 {
-
 
     return sockfd;
 }
 
-
-void ETH :: Transmit(char* Buffer)
+void ETH ::Transmit(char *Buffer)
 {
 
-
-     int state = write(sockfd,Buffer,strlen(Buffer)); 
-     if( state == -1 )
-     {
-
-        cout << "--- Fail To Transmit Ethernet Data to Server ---" << endl; 
-
-     }
-
-
-}
-
-char ETH :: Receive()
-{
-    
-     char c;
-     int state = read(sockfd,&c,1); 
-     if( state == -1 )
-     {
-
-        cout << "--- Fail To Receive Ethernet Data From Server ---" << endl; 
-
-     }
-
-
-}
-
-void ETH :: Receive(char* Buffer, int size)
-{
-
-    int state = read(sockfd,Buffer,size);
-    if(state == -1)
+    int state = write(sockfd, Buffer, strlen(Buffer));
+    if (state == -1)
     {
 
-        cout <<"--- Fail To Receive Ethernet Data From Server ---" << endl; 
-
+        cout << "--- Fail To Transmit Ethernet Data to Server ---" << endl;
     }
-
-
-
-}
-ETH :: ~ETH()
-{
-
-     cout << "--- Destructing Object : " << this << " ---" << endl;  
-     close(sockfd);
-
 }
 
-ETH* ETH :: Get_This()
+char ETH ::Receive()
 {
 
-return this; 
+    char c;
+    int state = read(sockfd, &c, 1);
+    if (state == -1)
+    {
 
+        cout << "--- Fail To Receive Ethernet Data From Server ---" << endl;
+    }
 }
 
-
-void *KeyButton_Rx_Task_Thread(void *);
-void *ETH_Rx_Task_Thread(void *argu);
-ETH *pETH; 
-
-
-
-int main(int argc, char* argv[])
+void ETH ::Receive(char *Buffer, int size)
 {
 
+    int state = read(sockfd, Buffer, size);
+    if (state == -1)
+    {
 
+        cout << "--- Fail To Receive Ethernet Data From Server ---" << endl;
+    }
+}
+ETH ::~ETH()
+{
 
-cout << "Hello World...!!!" <<endl; 
-cout << "clinet...!!!" <<endl; 
+    cout << "--- Destructing Object : " << this << " ---" << endl;
+    close(sockfd);
+}
 
+ETH *ETH ::Get_This()
+{
 
-    char* Address = new char[1024];
+    return this;
+}
+
+void *Rx_Key_Thread(void *argu);
+void *Rx_Msg_Thread(void *argu);
+void Error_Handle();
+
+pthread_t Rx_Key_Tid;
+pthread_t Rx_Msg_Tid;
+
+ETH *pETH;
+
+int main(int argc, char *argv[])
+{
+
+    char Address[1024];
     int Port;
 
-
-    pthread_t ETH_Rx_Task_Thread_TID;
-    pthread_t KeyButton_Rx_Task_Thread_TID;
-
     cout << "Server Address : ";
-    cin >> Address; 
+    cin >> Address;
 
-    cout <<"Server Port : ";
-    cin >> Port; 
-   
+    cout << "Server Port : ";
+    cin >> Port;
 
-    pETH = new ETH( Address,Port ); 
+    pETH = new ETH(Address, Port);
 
-    // pETH->Transmit("Hello I'm Clinet ...!!!\n");
 
-    cout << "pETH : " << (int *)pETH << endl;
-    cout << "pETH->Get_This() : " << (int *)pETH->Get_This() << endl;
+    if ( pthread_create(&Rx_Key_Tid,NULL,Rx_Key_Thread,NULL) == 0 ) printf("Create Rx_Key_Thread Routine : [ OK ]\n");
+    else Error_Handle(); 
+    if(pthread_create(&Rx_Msg_Tid,NULL,Rx_Msg_Thread,NULL) == 0 ) printf("Create Rx_Msg_Thread Routine : [ OK ]\n"); 
+    else Error_Handle(); 
 
-    pthread_create(&ETH_Rx_Task_Thread_TID, NULL, &ETH_Rx_Task_Thread, NULL);
-    pthread_create(&KeyButton_Rx_Task_Thread_TID, NULL, &KeyButton_Rx_Task_Thread, NULL);
 
-    while(1)
-    {
+    if( pthread_join(Rx_Key_Tid,NULL) == 0 ) printf("Join Rx_Key_Thread Routine : [ OK ]\n"); 
+    else Error_Handle();
 
 
 
-    }
+    printf("Exit The Program\n"); 
+    delete pETH; 
+    return 0;
 
-    pthread_join(ETH_Rx_Task_Thread_TID, NULL);
-    pthread_join(KeyButton_Rx_Task_Thread_TID, NULL);
-
-
-
-    return 0; 
+  
 }
 
-
-
-void *KeyButton_Rx_Task_Thread(void *argu)
+void *Rx_Key_Thread(void *argu)
 {
 
-
-    cout << "---  KeyButton Rx Task Thread Entry Point ---" << endl;
+    printf("Under Rx_Key_Thread Routine\n");
     char Buffer[1024];
-    int sockfd = pETH->GetSockFD(); 
-    int state; 
-    
-    
-    bzero(Buffer,sizeof(char)*1024);
+    int SocketFD = pETH->GetSockFD();
+    int state;
+    char c; 
+    int i = 0; 
 
-    while(1)
-    {
-
-        cout << "Message For Host : ";
-        cin >> Buffer;
-
-        state = write(sockfd,Buffer,1024); 
-        if( state != -1 )
-        {
-
-            bzero(Buffer,1024); 
-            cout << "write ok" << endl; 
-        }
-        
-
-    }
-
-   
-
-       
-
-    return NULL;
-}
-
-void *ETH_Rx_Task_Thread(void *argu)
-{
-
-    cout << "---  ETH Receive Task Thread Entry Point ---" << endl;
-    int idx = 0;
-    char *Buffer = new char[1024];
-    char c;
-
-    memset(Buffer, 0x0, 1024);
+    bzero(Buffer, 1024); 
 
     while (1)
     {
 
-        c = pETH->Receive();
-        Buffer[idx++] = c;
-
-        if (c == '\b')
-        {
-            putchar(' ');
-        }
-        else if (c == '\n')
+        c = getchar();
+        Buffer[i++] = c;
+        if( c == '\b') putchar(' ');
+        else if ( c == '\n')
         {
 
-            cout << "From Vim : " << Buffer;
-
-            if (strcmp("quit\n", Buffer) == 0)
+            Buffer[--i] = '\0';
+            printf("Console : %s\n",Buffer);
+            write(SocketFD,Buffer,strlen(Buffer));
+            if( strcmp(Buffer,"quit") == 0 )
             {
-                cout << "--- Exit Process ---" << endl;
 
-                memset(Buffer, 0x0, sizeof(Buffer) * 1024);
-                delete pETH;
-                delete[] Buffer;
+                if ( shutdown(SocketFD,SHUT_RDWR) == 0 ) printf("Shutdown SocketFD Write/Read Stream : [ OK ]\n");
+                else Error_Handle();
 
-                exit(1);
+
+                
             }
+            bzero(Buffer,1024);
+            i = 0 ; 
 
-            bzero(Buffer, sizeof(Buffer));
-            idx = 0;
+        }
+    
+
+    }
+
+
+    printf("End of Rx_Key_Thread Routine\n"); 
+
+ 
+}
+
+void *Rx_Msg_Thread(void *argu)
+{
+
+    printf("Under Rx_Key_Thread Routine\n");
+    
+    char Buffer[1024];
+    char c;
+    int SocketFD = pETH->GetSockFD();
+
+    while (1)
+    {
+
+        bzero(Buffer, 1024);
+        int Status = read(SocketFD, Buffer, 1024);
+
+        if ((Status != 0) && (Status != -1))
+        {
+
+            printf("From Host : %s\n", Buffer);
+        }
+        else if ((Status == 0) || (Status == -1))
+        {
+
+            Error_Handle();
+            break;
         }
     }
 
-    return argu;
+  
+    if( close(SocketFD) == 0 ) printf("Close Socket : [ OK ]\n");
+    else Error_Handle(); 
+    printf("End of Rx_Msg_Thread Routine\n");
+
+
+}
+void Error_Handle()
+{
+
+    char *Msg = new char[1024];
+    bzero(Msg, 1024);
+    sprintf(Msg, "Error Code < %d > ", errno);
+    perror(Msg);
+    delete[] Msg;
 }
